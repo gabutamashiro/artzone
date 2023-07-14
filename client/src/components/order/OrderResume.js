d5473e9118078812763420b6aa2737b9c5cdb79a
gabutamashiro <gaby.tamashiro@gmail.com>
2023-07-14 00:02:05 -0300

    feat: add updates order resume

--- client/src/components/order/OrderResume.js
+++ client/src/components/order/OrderResume.js
@@ -11,13 +11,19 @@ const OrderResume = (props) => {
 
   const [order, setOrder] = useState(null);
 
+  const [paymentMethod, setPaymentMethod] = useState(null);
+
+  const [orderProduct, setOrderProduct] = useState(null);
+
   let loadOrder = null;
+  let formatPaymentMethod = null;
+  let loadOrderProduct = null;
 
   useEffect(() => {
     if (orderId){
       loadOrder()
     }
-	}, [loadOrder, orderId])
+    }, [loadOrder, orderId])
 
   loadOrder = useCallback(async () => {
     try {
@@ -25,11 +31,51 @@ const OrderResume = (props) => {
       const url = `http://localhost:8080/orders/${orderId}`;
       const response = await axios.get(url);
       setOrder(() => response.data[0]);
+      formatPaymentMethod(response.data[0].payment_method)
+      loadOrderProduct(response.data[0].product_id)
       setIsLoading(false);
     } catch (error) {
       setIsLoading(false);
     }
-	}, [setIsLoading, orderId])
+    }, [setIsLoading, orderId, formatPaymentMethod, loadOrderProduct])
+
+  formatPaymentMethod = useCallback((payment_method) => {
+    switch(payment_method) {
+      case 'cartao':
+        setPaymentMethod('Cartão de crédito')
+        break
+      case 'pix':
+        setPaymentMethod('Pix')
+        break
+      case 'boleto':
+        setPaymentMethod('Boleto')
+        break
+      default:
+        setPaymentMethod(payment_method)
+        break
+    }
+  }, [setPaymentMethod])
+
+  loadOrderProduct = useCallback(async (orderProductId) => {
+		if (!orderProductId) {
+			return;
+		}
+		try {
+			setIsLoading(true);
+			const url = `http://localhost:8080/products/${orderProductId}`;
+			const response = await axios.get(url);
+			if (response && response.data && response.data.message) {
+				alert(response.data.message);
+				setIsLoading(false);
+				return;
+			} else {
+				setOrderProduct(response.data[0]);
+				setIsLoading(false);
+			}
+		} catch (error) {
+			setIsLoading(false);
+		}
+	}, [setIsLoading]);
 
   return (
     <div>
@@ -44,6 +90,10 @@ const OrderResume = (props) => {
             <h3>Informações Do Pedido</h3>
             <div className="order-resume__data">
               <span>ID: {order?.id}</span>
+              <span>Total: R$ {order?.product_price}</span>
+              <span>Forma de pagamento: {paymentMethod}</span>
+              <span>Nome do destinatário: {order?.delivery_name}</span>
+              <span>E-mail do destinatário: {order?.delivery_email}</span>
             </div>
           </div>
         </div>
